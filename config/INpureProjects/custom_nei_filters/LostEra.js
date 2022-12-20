965baff422fdf1cdf5fad71518afedc0c07b53ac
SwedishKaito <wik1998@hotmail.se>
2022-12-20 21:14:27 +0100

    Hid Items from NEI + Updated Localization
    - Hid Untextured and Unobtainable Block
    - Updated broken Localizaton

--- config/INpureProjects/custom_nei_filters/LostEra.js
+++ config/INpureProjects/custom_nei_filters/LostEra.js
@@ -115,4 +115,15 @@ if (FML.isModLoaded("pokepatch")) {
     NEI.hide("NVLWarpdriveExtensions:NVLItemLTFBControl_15");
     NEI.hide("NVLWarpdriveExtensions:NVLItemLTFBControl_16");
     NEI.hide("NVLWarpdriveExtensions:NVLItemLTFBControl_17");
+    NEI.hide("chisel:bloodBrick");
+    NEI.hide("chisel:aluminum_stairs.0");
+    NEI.hide("chisel:aluminum_stairs.1");
+    NEI.hide("chisel:aluminum_stairs.2");
+    NEI.hide("chisel:aluminum_stairs.3");
+    NEI.hide("chisel:aluminum_stairs.4");
+    NEI.hide("chisel:aluminum_stairs.5");
+    NEI.hide("chisel:aluminum_stairs.6");
+    NEI.hide("chisel:aluminum_stairs.7");
+    NEI.hide("chisel:aluminum_stairs.8");
+
 }
